1d24ff770029de5ccdc37e11443996788c9f0598
SNY-AC\Acer <shinya.umd@gmail.com>
2022-01-04 11:42:43 +0900

    no message

--- ファイル名変更ツール00/std.js
+++ ファイル名変更ツール00/std.js
@@ -1,21 +1,69 @@
-function Ev_SelectFolder(id0) {
-    alert("EV");
-    let files = document.getElementById(id0).target.files;
 
-    for (let i = 0; i < files.length; i++) {
-        alert(files[i])
-    }
-}
+SelectedDir = "";
+
+
+document.getElementById('SelectDir').addEventListener('change', CheckDir);
+
+
+
 
 function debug0() {
-    alert("debug0");
-    // String path1 = "D:\";
-    // File file1 = new File(path1);
-    // File fileArray1[] = file1.listFiles();
-    // alert(fileArray1[0].toString);
+    // selectタグを取得する
+    var select = document.getElementById("sampleSelect");
+    // optionタグを作成する
+    var option = document.createElement("option");
+    // optionタグのテキストを4に設定する
+    option.text = 4;
+    // optionタグのvalueを4に設定する
+    option.value = 4;
+    // selectタグの子要素にoptionタグを追加する
+    select.appendChild(option);
 }
 
 function debug1() {
-    alert("debug1");
+    // var loc = window.location.pathname;
+    // var dir = loc.substring(0, loc.lastIndexOf('/'));
+    // alert(dir);
 
 }
+
+function GetDirName(target)
+{
+  const path = require('path');
+  const result = path.dirname(target);
+  return result;
+}
+
+
+
+function CheckDir(){
+  SelDirFiles = document.getElementById('SelectDir');
+  SelectedDir = SelDirFiles.files[0].webkitRelativePath;
+  alert(SelectedDir);
+  files = SelDirFiles.files;
+  for(i = 0; i < files.length; i++)
+    alert(files[i].name);
+}
+
+
+
+
+// document.getElementById('file').addEventListener('change', ev => {
+//   alert();
+//   for (let i = 0; i < ev.target.files.length; i++) {
+//
+//       let file = ev.target.files[i];
+//       alert(file.name);
+//       // ディレクトリの相対パス
+//       let relativePath = file.webkitRelativePath;
+//       // ここではテキストファイルとして読み出してみる.
+//       let fileReader = new FileReader();
+//       fileReader.onload = event => {
+//           // 内容を取得する.
+//           let text = event.target.result;
+//           // 表示してみる.
+//           console.log(relativePath, text);
+//       }
+//       fileReader.readAsText(file);
+//   }
+// });
